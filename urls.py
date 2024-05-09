9d88c38685a66e9d23f3b0f45e4557603731dc20
Vaishali Khandelwal <133322828+VishKhandelwal@users.noreply.github.com>
2024-05-09 08:59:50 +0530

    Vendorapp

--- urls.py
+++ urls.py
@@ -1,28 +1,14 @@
-"""
-URL configuration for venderprofile project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
-from rest_framework.authtoken.views import obtain_auth_token
+from django.urls import path
+from .views import *
 
 
+urlpatterns = [ 
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('venderapp.urls')),
-    path('auth/',include('rest_framework.urls',namespace='rest_framework')), 
-    path('api-token-auth/', obtain_auth_token, name='api_token_auth'), 
+    path('', VendorListCreateView.as_view(), name='vendor-list-create'),
+    path('', VendorRetrieveUpdateDeleteView.as_view(), name='vendor-retrieve-update-delete'),
+    path('', PurchaseOrderListCreateView.as_view(), name='purchase-order-list-create'),
+    path('', PurchaseOrderRetrieveUpdateDeleteView.as_view(), name='purchase-order-retrieve-update-delete'),
+    path('', VendorPerformanceView.as_view(), name='vendor-performance'),
+    path('', AcknowledgePurchaseOrderView.as_view(), name='acknowledge-purchase-order'),
 ]
